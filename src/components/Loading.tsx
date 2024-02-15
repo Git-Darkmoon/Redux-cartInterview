11a370f8ef892bd46f068192b56056ad0fc66cc1
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-14 19:16:56 -0500

    feat: loading spinner component.

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,13 @@
 function Loading() {
   return (
-    <div className="w-full h-[calc(100vh-3rem)] flex items-center justify-center">
-      <h1 className="font-medium text-6xl text-slate-800">Loading ...</h1>
+    <div className="w-full pt-16 flex items-center justify-center text-3xl gap-4">
+      <div
+        className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+        role="status"
+      >
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
+      </div>
+      Loading...
     </div>
   )
 }
