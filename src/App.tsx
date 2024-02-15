d42e439727b6f3204d22ef3d1e5b328be749f2d3
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-14 19:16:32 -0500

    feat: BrowserRouter setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import Navbar from "./components/Navbar"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import ProductsContainer from "./components/ProductsContainer"
+import HomeLayout from "./components/HomeLayout"
+import Cart from "./components/Cart"
+import ErrorPage from "./pages/ErrorPage"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <ProductsContainer />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
+  },
+])
 
 function App() {
-  return (
-    <main className="min-h-screen bg-slate-200 w-full px-12">
-      <Navbar />
-      <ProductsContainer />
-    </main>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
