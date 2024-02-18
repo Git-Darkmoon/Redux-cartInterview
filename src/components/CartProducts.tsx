b0dea963dc2a927e8af4ed29777372ce524cad4c
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-18 10:08:33 -0500

    styles: new UI

--- src/components/CartProducts.tsx
+++ src/components/CartProducts.tsx
@@ -1,6 +1,7 @@
-import { clearCart } from "../features/cart/cartSlice"
+import { clearCart, removeFromCart } from "../features/cart/cartSlice"
 import { useAppDispatch, useAppSelector } from "../hooks"
 import { formatPrice } from "../utils"
+import CartProduct from "./CartProduct"
 
 function CartProducts() {
   const dispatch = useAppDispatch()
@@ -8,35 +9,19 @@ function CartProducts() {
 
   return (
     <main className="grid grid-cols-12 gap-4 px-6 py-3">
-      <article className="col-span-8 bg-slate-100 shadow-md rounded-md p-4">
+      <article className="col-span-8 bg-slate-50 shadow-md rounded-md p-4">
         <h1 className="text-3xl mb-4">Cart Items</h1>
-        <section className="grid grid-cols-3 gap-6 ">
+        <section className="flex flex-col items-center gap-6 gap-y-10 ">
           {amount <= 0 ? (
             <h1>Cart Empty</h1>
           ) : (
             cartItems.map((item) => {
-              return (
-                <>
-                  <div className="">
-                    <h2>{item.title}</h2>
-                    <img
-                      className="size-20 object-contain mix-blend-multiply"
-                      src={item.image}
-                      alt={item.title}
-                      loading="lazy"
-                      width={300}
-                      height={300}
-                    />
-                  </div>
-                  <div className=" ">{item.category}</div>
-                  <div className=" ">{formatPrice(item.price)}</div>
-                </>
-              )
+              return <CartProduct key={item.id} {...item} />
             })
           )}
         </section>
       </article>
-      <article className="col-span-4 bg-slate-100 shadow-md rounded-md p-4">
+      <article className="col-span-4 bg-slate-50 shadow-md rounded-md p-4">
         <h1 className="text-3xl mb-4">Checkout</h1>
         <div className="w-96 h-36 bg-green-400"></div>
       </article>
