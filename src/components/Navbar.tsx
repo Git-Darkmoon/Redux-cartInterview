87135a204d61b042985b0510349866dde38abb15
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-20 17:35:42 -0500

    styles: updated UI.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,24 @@ import { Link } from "react-router-dom"
 import { useAppSelector } from "../hooks"
 
 function Navbar() {
-  const { cartItems, total } = useAppSelector((store) => store.cart)
+  const { amount } = useAppSelector((store) => store.cart)
 
   return (
     <div className="h-16 flex items-center justify-between w-full">
-      <div>
-        <div>Items in cart: {cartItems.length}</div>
-        <div>Subtotal: $ {total.toFixed(2)}</div>
-      </div>
+      <Link
+        className="cursor-pointer hover:underline underline-offset-2"
+        to="/"
+      >
+        All products
+      </Link>
       <div className="flex flex-col items-end justify-between">
         <Link
-          className="cursor-pointer hover:underline underline-offset-2"
+          className="relative cursor-pointer hover:underline underline-offset-2"
           to="/cart"
         >
+          <div className="absolute -top-3 -right-3 bg-purple-400 p-2.5 size-4 text-lg rounded-full flex items-center justify-center">
+            {amount}
+          </div>
           Cart
         </Link>
       </div>
