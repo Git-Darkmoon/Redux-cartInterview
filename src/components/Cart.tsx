b2e4bf241df702801dce110a03d1125b0c57fbcd
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-20 17:34:31 -0500

    refactor: unused import.

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,15 +1,8 @@
-import { Link } from "react-router-dom"
 import CartProducts from "./CartProducts"
 
 function Cart() {
   return (
     <>
-      <Link
-        className="cursor-pointer hover:underline underline-offset-2"
-        to="/"
-      >
-        Go back to products
-      </Link>
       <CartProducts />
     </>
   )
