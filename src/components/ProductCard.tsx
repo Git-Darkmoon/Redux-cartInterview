6ea4baf2c72c0658feb968ad3f8f2cfc17a794ee
Nicolas Mayorga <namayorgav@gmail.com>
2024-02-20 17:36:10 -0500

    styles: updated UI.

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -9,7 +9,7 @@ function ProductCard(productInfo: Product) {
   const dispatch = useAppDispatch()
 
   return (
-    <article className="bg-slate-50 w-xs flex flex-col gap-4 justify-between shadow-lg p-6 rounded-md hover:scale-105 hover:shadow-xl transition">
+    <article className="bg-slate-50 w-xs flex flex-col gap-2 justify-between shadow-lg p-6 rounded-md hover:shadow-2xl transition">
       <img
         src={productInfo.image}
         alt={productInfo.title}
@@ -19,29 +19,28 @@ function ProductCard(productInfo: Product) {
         className="size-48 object-contain self-center mix-blend-multiply"
       />
       <h2 className="text-xl font-bold text-slate-900">{productInfo.title}</h2>
-
+      <p className=" text-sm text-gray-800/70 font-semibold">
+        {productInfo.category}
+      </p>
       <p className="text-slate-900">
         {showMore
           ? `${productInfo.description} `
           : `${productInfo.description.slice(0, 80)}... `}
         <button
-          className="text-blue-600 hover:underline underline-offset-2 rounded-sm"
+          className="text-purple-600 hover:underline underline-offset-2 rounded-sm"
           onClick={() => setShowMore(!showMore)}
         >
           {showMore ? "show less" : "show more"}
         </button>
       </p>
-      <div className="flex text-sm justify-between text-gray-800/70 font-semibold">
-        <div>{productInfo.category}</div>
-        <div>⭐ {productInfo.rating.rate}</div>
-      </div>
+
       <div className="flex space-x-4 items-center justify-between">
-        <h3 className="text-xl font-mono">{`${formatPrice(
+        <h3 className="text-xl text-purple-500 font-bold font-sans">{`${formatPrice(
           productInfo.price
         )}`}</h3>
         <button
           onClick={() => dispatch(addToCart(productInfo))}
-          className="bg-blue-400 px-3 py-2 text-base font-semibold hover:bg-blue-500 rounded-sm transition-colors"
+          className="bg-gradient-to-tl from-purple-400 via-purple-200 to-purple-400 hover:scale-105 px-10 py-2 text-base font-semibold transition"
         >
           Add to cart
         </button>
